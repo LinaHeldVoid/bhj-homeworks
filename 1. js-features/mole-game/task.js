043e0459978b3b9ad89efbb9dba8934b00bd11e9
Lina_Held_Void <iltep118666@gmail.com>
2023-11-01 20:48:09 +0400

    Неполадки с 3 задачей

--- 1. js-features/mole-game/task.js
+++ 1. js-features/mole-game/task.js
@@ -6,15 +6,21 @@ function getHole(index) {
 	return hole;
 };
 
-while (Number(dead.textContent) < 10 && Number(lost.textContent) < 5) {
-	for (let i = 0; i < 9; i++) {
-		hole = getHole[i];
-		hole.onclick = () => {
-			if (hole.className.includes("hole_has-mole")) {
-				dead.textContent = Number(dead.textContent) + 1;
-			} else {
-				lost.textContent = Number(lost.textContent) + 1;
+for (let i = 1; i < 10; i++) {
+	hole = getHole(i);
+	hole.onclick = () => {
+		if (hole.classList.contains( 'hole_has-mole' )) {
+			dead.textContent = Number(dead.textContent) + 1;
+			if (Number(dead.textContent) == 10) {
+				alert("Поздравляем, вы выиграли!")
+				dead = 0
+			};
+		} else {
+			lost.textContent = Number(lost.textContent) + 1;
+			if (Number(lost.textContent) == 5) {
+				alert("Сожалеем, вы проиграли(")
+				lost = 0
 			};
 		};
-	}
+	};
 };
