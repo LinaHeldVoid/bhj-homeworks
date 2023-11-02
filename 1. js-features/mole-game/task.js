90e672c7b5f7e935b083a23e08a231452a289dbd
Lina_Held_Void <iltep118666@gmail.com>
2023-11-02 17:33:43 +0400

    Готовы три задания

--- 1. js-features/mole-game/task.js
+++ 1. js-features/mole-game/task.js
@@ -1,5 +1,5 @@
-const dead = document.getElementById("dead");
-const lost = document.getElementById("lost");
+dead = document.getElementById("dead");
+lost = document.getElementById("lost");
 
 function getHole(index) {
 	const hole = document.getElementById(`hole${index}`);
@@ -7,19 +7,21 @@ function getHole(index) {
 };
 
 for (let i = 1; i < 10; i++) {
-	hole = getHole(i);
+	const hole = getHole(i);
 	hole.onclick = () => {
 		if (hole.classList.contains( 'hole_has-mole' )) {
 			dead.textContent = Number(dead.textContent) + 1;
 			if (Number(dead.textContent) == 10) {
 				alert("Поздравляем, вы выиграли!")
-				dead = 0
+				dead.textContent = 0
+				lost.textContent = 0
 			};
 		} else {
 			lost.textContent = Number(lost.textContent) + 1;
 			if (Number(lost.textContent) == 5) {
 				alert("Сожалеем, вы проиграли(")
-				lost = 0
+				dead.textContent = 0
+				lost.textContent = 0
 			};
 		};
 	};
