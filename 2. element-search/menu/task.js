0ff2949a42658d38833050f4116d53c2eaeda43f
Lina_Held_Void <iltep118666@gmail.com>
2023-12-08 17:08:47 +0400

    Готово 2 задание

--- 2. element-search/menu/task.js
+++ 2. element-search/menu/task.js
@@ -1,14 +1,30 @@
 const menuBar = document.querySelectorAll('.menu__link');
 
-for (menu of Array.from(menuBar)) {
+// function closeSuBs() {
+// 	const subs = document.querySelectorAll('.menu_sub');
+
+// 	for (let sub of Array.from(subs)) {
+// 		if sub.classList.includes('menu_active') {
+// 			sub.classList.toggle('menu_active');
+// 		}
+
+// 		return false;
+// 	}
+// 	return;
+// }
+
+for (let menu of Array.from(menuBar)) {
 
 	menu.onclick = () => {
-		let menuSub = menu.querySelector('.menu_sub');
+		const subList = menu.closest('.menu__item');
+		menuSub = subList.querySelector('.menu_sub');
 
             if (menuSub) {
+            	// closeSuBs
                 menuSub.classList.toggle("menu_active");
             }
+
+            return false;
+
 	}
 }
-
-alert(menuBar.textContent)
