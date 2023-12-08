56eb5caf1df5244dc08e6668c6dc6771a0bfea67
Lina_Held_Void <iltep118666@gmail.com>
2023-12-09 01:12:28 +0400

    Начато задание 3

---
+++ 2. element-search/slider/task.js
@@ -0,0 +1,23 @@
+const arrows = document.querySelectorAll('.slider__arrow');
+const arrowsArr = Array.from(arrows);
+
+
+function prewSlide() {
+
+};
+
+function nextSlide() {
+
+};
+
+function slider() {
+
+	arrowsArr[0].onclick = () => {
+		// previous
+	}
+
+	arrowsArr[1].onclick = () => {
+		// next
+	}
+
+};
