9d1c09574f0a44f0503eba696fdb93f89d80c628
Lina_Held_Void <iltep118666@gmail.com>
2023-12-17 00:29:56 +0400

    Вопрос по заданию 3

--- 2. element-search/slider/task.js
+++ 2. element-search/slider/task.js
@@ -1,23 +1,56 @@
 const arrows = document.querySelectorAll('.slider__arrow');
-const arrowsArr = Array.from(arrows);
+const slides = document.querySelectorAll('.slider__item');
+const slidesArr = Array.from(slides);
 
-
-function prewSlide() {
-
-};
-
-function nextSlide() {
-
-};
 
 function slider() {
 
-	arrowsArr[0].onclick = () => {
-		// previous
+	function prewSlide() {
+		for (let slide of slidesArr) {
+			if (slide.classList.contains('slider__item_active')) {
+				let num = slidesArr.indexOf(slide);
+				slide.classList.toggle('slider__item_active')
+
+				if (slidesArr[num - 1]) {
+					slidesArr[num - 1].classList.toggle('slider__item_active')
+				} else {
+					slidesArr[slidesArr.length - 1].classList.toggle('slider__item_active')
+				}
+			}
+		}
 	}
 
-	arrowsArr[1].onclick = () => {
-		// next
+	function nextSlide(number) {
+		for (let slide of slidesArr) {
+			if (slide.classList.contains('slider__item_active')) {
+				let num = slidesArr.indexOf(slide);
+				slide.classList.toggle('slider__item_active')
+
+				if (slidesArr[num + 1]) {
+					slidesArr[num + 1].classList.toggle('slider__item_active')
+				} else {
+					slidesArr[0].classList.toggle('slider__item_active')
+				}
+			}
+		}
+
 	}
 
-};
+
+	for (let arrow of Array.from(arrows)) {
+
+		arrow.onclick = () => {
+			// alert(arrow.classList)
+
+			if (arrow.classList.contains('slider__arrow_next')) {
+				nextSlide();
+			}
+			else {
+				prewSlide();
+			}
+		}
+	}
+
+}
+
+slider();
